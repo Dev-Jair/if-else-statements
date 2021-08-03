ebdcbdfea443982386ebc8c284d294d6669fd8bf
Dev-Jair <86578182+Dev-Jair@users.noreply.github.com>
2021-08-03 18:10:54 +0200

    Add files via upload

---
+++ Opdracht_if_else_statements/script.js
@@ -0,0 +1,26 @@
+const age = 22;
+
+if (age > 18) {
+
+    console.log("Je mag naar binnen!")
+} else {
+    console.log("Helaas! Je kan niet naar binnen.")
+}
+
+const isFemale = true;
+
+if (isFemale == false) {
+
+    console.log("Ready voor de Ladiesnight?!!")
+} else {
+    console.log("Deze tickets zijn alleen beschikbaar voor de dames!")
+}
+
+const driverStatus = "Bob";
+
+if (driverStatus) {
+
+    console.log("Mooi, je mag rijden!")
+} else {
+    console.log("Let op! Je mag niet rijden.")
+}
